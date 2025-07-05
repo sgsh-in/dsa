a490e87e174e661d7f9768811d0e77d8bab37c90
Sayantan Ghosh <sayantan.ghosh03@gmail.com>
2025-07-05 19:25:21 +0530

    Added few more methods to the singly LinkedList class

--- src/LinkedList/index.js
+++ src/LinkedList/index.js
@@ -28,6 +28,7 @@ class LinkedList {
   }
 
   /**
+   * @description push a new node to the end of a SinglyLinkedList
    * @param {*} value
    * @returns {this} linked list instance
    */
@@ -49,6 +50,80 @@ class LinkedList {
   }
 
   /**
+   * @description pop a node (if possible) from the end of a SinglyLinkedList
+   * @returns the popped node
+   */
+  pop() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    let prev = temp; // to track the second last node
+    while (temp.next !== null) {
+      // traverse till temp points to the last node and the prev pointer points to the second last node
+      prev = temp;
+      temp = temp.next;
+    }
+    // point the tail to the second last node
+    this.tail = prev;
+    this.tail.next = null; // detach the last node
+    --this.length;
+    if (this.length === 0) {
+      /**
+       * if linked list only has 1 node, after the
+       * reduction of length, we will see that the head and tail
+       * still points to the removed node
+       */
+      this.head = null;
+      this.tail = null;
+    }
+    // return the popped node
+    return temp;
+  }
+
+  /**
+   * @description insert a node at the beginning of the SinglyLinkedList
+   * @param {*} value
+   * @returns {this} linked list instance
+   */
+  unshift(value) {
+    // create a new node
+    const newNode = new Node(value);
+    // if the linked list is empty
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // point the new node's next pointer to the head
+      newNode.next = this.head;
+      // update the head pointer
+      this.head = newNode;
+    }
+    ++this.length;
+    return this;
+  }
+
+  /**
+   * @description remove an item from the beginning of the SinglyLinkedList
+   * @returns the removed node
+   */
+  shift() {
+    // if the list is empty
+    if (!this.head) return undefined;
+    const temp = this.head;
+    this.head = temp.next;
+    temp.next = null;
+    this.length--;
+    if (this.length === 0) {
+      /**
+       * if linked list only has 1 node, after the
+       * reduction of length, we will see that the tail
+       * still points to the removed node
+       */
+      this.tail = null;
+    }
+    return temp;
+  }
+
+  /**
    * @description print all the nodes of the linked list
    */
   printList() {
@@ -65,7 +140,27 @@ class LinkedList {
 }
 
 // Tests
+// create the linked list
 const ll1 = new LinkedList();
+// push some items
+ll1.push(1);
 ll1.push(2);
 ll1.push(3);
+ll1.push(4);
+ll1.push(5);
+ll1.push(6);
+// print list
+ll1.printList();
+// pop 3 items
+ll1.pop();
+ll1.pop();
+ll1.pop();
+// push a new item
+ll1.push(9);
+// unshift 2 tems
+ll1.unshift(10);
+ll1.unshift(17);
+// shift 1 time
+ll1.shift();
+// print list
 ll1.printList();
